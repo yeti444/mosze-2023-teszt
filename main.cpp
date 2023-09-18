4d5bcfac923833b87791196981f283529af507e4
yeti444 <kristof20011020@gmail.com>
2023-09-18 10:37:58 +0200

    Hibák kikommentelve új branchre

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //ROSSZ VÁLTOZÓ
+    std::cout << '1-100 ertekek duplazasa' //CHAR KIÍRATÁS, DE STRINGET KELL 
+    for (int i = 0;) //BEFEJEZETLEN FOR
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //ROSSZ KIÍRÁS
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;  //NINCS INICIALIZÁLVA
+    for (int i = 0; i < N_ELEMENTS, i++) //ROSSZ FOR
     {
-        atlag += b[i]
+        atlag += b[i]   
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
