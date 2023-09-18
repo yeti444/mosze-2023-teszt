ddc05fa26f201a2f0ba720cde8bceab3da40985a
yeti444 <kristof20011020@gmail.com>
2023-09-18 11:28:45 +0200

    Hibák javítva

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //ROSSZ VÁLTOZÓ
-    std::cout << '1-100 ertekek duplazasa' //CHAR KIÍRATÁS, DE STRINGET KELL 
-    for (int i = 0;) //BEFEJEZETLEN FOR
+    int *b = new int[N_ELEMENTS]; //ROSSZ VÁLTOZÓ
+    std::cout << "1-100 ertekek duplazasa" << std::endl; //CHAR KIÍRATÁS, DE STRINGET KELL (+ hiányzó ;)
+    for (int i = 0; i < N_ELEMENTS; i++) //BEFEJEZETLEN FOR
     {
-        b[i] = i * 2;
+        b[i] = (i+1) * 2; //MIVEL AZ VOLT MEGADVA, HOGY 1-100, EZÉRT NEM JÓ A 0-99
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++) //ROSSZ FOR CIKLUS
     {
-        std::cout << "Ertek:" //ROSSZ KIÍRÁS
+        std::cout << "Ertek:" << b[i] << std::endl; //ROSSZ KIÍRÁS
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;  //NINCS INICIALIZÁLVA
-    for (int i = 0; i < N_ELEMENTS, i++) //ROSSZ FOR
+    int atlag = 0;  //NINCS INICIALIZÁLVA
+    for (int i = 0; i < N_ELEMENTS; i++) //ROSSZ FOR
     {
-        atlag += b[i]   
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
